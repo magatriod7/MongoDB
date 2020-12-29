42697f965cfa3ee6496a920c9a1b7d110487f663
JH <magatriod7@naver.com>
2020-12-29 13:05:15 +0900

    resetneed

--- server/routes/post.js
+++ server/routes/post.js
@@ -1,15 +1,64 @@
-import express from "express";
 
-import { getPost, postPost } from "../controllers/postController"
-// Model
-import Post from "../models/post";
-//몽구스 포스트 스키마 import
+const router = express.Router();
 
-const postRouter = express.Router();
+import multer from "multer";
+import multerS3 from "multer-s3";
+import path from "path";
+import AWS from "aws-sdk";
+import dotenv from "dotenv";
+dotenv.config();
+
+const s3 = new AWS.S3({
+  accessKeyId: process.env.AWS_KEY,
+  secretAccessKey: process.env.AWS_PRIVATE_KEY,
+});
+
+const uploadS3 = multer({
+  storage: multerS3({
+    s3,
+    bucket: "sideproject2020/upload",
+    region: "ap-northeast-2",
+    key(req, file, cb) {
+      const ext = path.extname(file.originalname);
+      const basename = path.basename(file.originalname, ext);
+      cb(null, basename + new Date().valueOf() + ext);
+    },
+  }),
+  limits: { fileSize: 100 * 1024 * 1024 },
+});
+
+// @route     POST api/post/image
+// @desc      Create a Post
+// @access    Private
+
+router.post("/image", uploadS3.array("upload", 5), async (req, res, next) => {
+  try {
+    console.log(req.files.map((v) => v.location));
+    res.json({ uploaded: true, url: req.files.map((v) => v.location) });
+  } catch (e) {
+    console.error(e);
+    res.json({ uploaded: false, url: null });
+  }
+});
 
 // api/post
-postRouter.get("/", getPost);
-
-postRouter.post("/", postPost);
-
-export default postRouter;
+router.get("/", async (req, res) => {
+  const postFindResult = await Post.find();
+  console.log(postFindResult, "All Post Get");
+  res.json(postFindResult);
+});
+router.post("/", auth, async (req, res, next) => {
+  try {
+    console.log(req, "req");
+    const { title, contents, fileUrl, creator } = req.body;
+    const newPost = await Post.create({
+      title,
+      contents,
+      fileUrl,
+      creator,
+    });
+    res.json(newPost);
+  } catch (e) {
+    console.log(e);
+  }
+});
