bfcac331a5c3232c2cb29e9dfe10aad96fc5aa75
JH <magatriod7@naver.com>
2021-05-08 01:01:27 +0900

    20210508 0101

--- build/server.js
+++ build/server.js
@@ -6,8 +6,12 @@ var _index = _interopRequireDefault(require("./config/index"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
-var PORT = _index["default"].PORT;
+var PORT = _index["default"].PORT,
+    JWT_SECRET = _index["default"].JWT_SECRET,
+    MONGO_URI = _index["default"].MONGO_URI;
 
 _app["default"].listen(PORT, function () {
   console.log("Server started on Port ".concat(PORT));
+  console.log("JWT_SECRET ".concat(JWT_SECRET));
+  console.log("MONGO_URI ".concat(MONGO_URI));
 });
--- server/server.js
+++ server/server.js
@@ -1,8 +1,11 @@
 import app from "./app";
 import config from "./config/index";
 
-const { PORT } = config;
+const { PORT, JWT_SECRET, MONGO_URI } = config;
 
 app.listen(PORT, () => {
   console.log(`Server started on Port ${PORT}`);
+  console.log(`JWT_SECRET ${JWT_SECRET}`);
+  console.log(`MONGO_URI ${MONGO_URI}`);
+
 });
